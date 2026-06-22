5a3edf1443582c3217fe05820be67ab73e7278dc
Emmanuel <davidemmanuelgaa@gmail.com>
2026-06-22 20:07:24 +0800

    Initial commit: this is still barebones

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-center text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
+
+
